b6cd67bf831ee8f42d7d3e9c6ea7868a20d8bf17
Rlss91 <rlss91@gmail.com>
2022-12-04 10:34:32 +0200

    dotenv

--- config/jwt.js
+++ config/jwt.js
@@ -2,28 +2,19 @@ const jwt = require("jsonwebtoken");
 
 const genToken = (payload, expDate = "30d") => {
   return new Promise((resolve, reject) => {
-    jwt.sign(
-      payload,
-      "2r34v09,kij-vt3u45098wm's90845b6mj9084y5",
-      { expiresIn: expDate },
-      (err, token) => {
-        if (err) reject(err);
-        else resolve(token);
-      }
-    );
+    jwt.sign(payload, process.env.JWT, { expiresIn: expDate }, (err, token) => {
+      if (err) reject(err);
+      else resolve(token);
+    });
   });
 };
 
 const verifyToken = (token) => {
   return new Promise((resolve, reject) => {
-    jwt.verify(
-      token,
-      "2r34v09,kij-vt3u45098wm's90845b6mj9084y5",
-      (err, payload) => {
-        if (err) reject(err);
-        else resolve(payload);
-      }
-    );
+    jwt.verify(token, process.env.JWT, (err, payload) => {
+      if (err) reject(err);
+      else resolve(payload);
+    });
   });
 };
 
--- models/connectToDB.js
+++ models/connectToDB.js
@@ -1,3 +1,4 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.connect("mongodb://127.0.0.1:27017/bizcardsdb");
+module.exports = mongoose.connect(process.env.CONSTR);
+// module.exports = mongoose.connect("mongodb://127.0.0.1:27017/bizcardsdb");
